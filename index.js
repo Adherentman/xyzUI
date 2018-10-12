6a381647939a3529ad4c332fa045a0d223b92531
徐梓昊 <adherentxu@gmail.com>
2018-10-12 23:55:22 +0800

    commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const tools = require('./packages');
-
-console.log(tools)
-let a = Object.keys(tools)
-console.log(a)
-a.forEach((item) => {
-  console.log(item)
-})
--- packages/index.js
+++
@@ -1,7 +0,0 @@
-const xyzButton = require('./xyz-component-button');
-const xyzCard = require('./xyz-component-card');
-
-module.exports ={
-  xyzButton,
-  xyzCard
-}
