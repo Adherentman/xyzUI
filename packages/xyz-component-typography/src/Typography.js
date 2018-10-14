c439384a2978eb5a89845ce0beecba7a20ffa87e
徐梓昊 <adherentxu@gmail.com>
2018-10-15 00:30:46 +0800

    add editorconfig

--- packages/xyz-component-typography/src/Typography.js
+++ packages/xyz-component-typography/src/Typography.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 class Typography extends React.Component {
-  render() {
-    return <div>typography</div>
-  }
+	render() {
+		return (
+			<div>
+				<a href="/">nihao</a>
+				typography
+			</div>
+		)
+	}
 }
 
 export default Typography
