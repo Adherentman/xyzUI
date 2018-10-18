2bf1565bac2ecc9989b632e97bc271e767e5869a
徐梓昊 <adherentxu@gmail.com>
2018-10-18 21:12:44 +0800

    create templete

--- packages/xyz-cli/lib/create.js
+++ packages/xyz-cli/lib/create.js
@@ -4,10 +4,37 @@ const fs = require('fs-extra')
 const { log, error } = console
 const cwd = process.cwd()
 
+const webpackFile = `const path = require('path')
+
+module.exports = {
+	entry: './index.js',
+	output: {
+		path: path.join(__dirname, 'lib'),
+		file: 'xyz-component',
+		library: '',
+		libraryTarget: 'umd',
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+	},
+}
+`
+
+async function writeWebpackConfigFile(componentName) {
+	try {
+		await fs.outputFile(
+			`${cwd}/${componentName}/${componentName}.webpack.js`,
+			webpackFile
+		)
+	} catch (err) {
+		error(err)
+	}
+}
 async function create(componentName) {
 	log(chalk.green(`create ${componentName} Start!`))
 	try {
 		await fs.ensureDir(`${cwd}/${componentName}`)
+		await writeWebpackConfigFile(componentName)
 		log(chalk.cyan('Create success'))
 	} catch (err) {
 		error(err)
--- packages/xyz-component-typography/src/Typography.js
+++ packages/xyz-component-typography/src/Typography.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Typography = styled.div``
 
 class TypographyComponent extends React.Component {
 	render() {
 		return (
 			<div>
 				<a href="/">test4！</a>
-				<Typography />
 			</div>
 		)
 	}
