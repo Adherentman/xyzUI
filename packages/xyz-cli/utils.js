7d982c7056041c6ae7a7470949a0cd4e0323320b
徐梓昊 <adherentxu@gmail.com>
2018-10-20 14:22:54 +0800

    format

--- packages/xyz-cli/utils.js
+++ packages/xyz-cli/utils.js
@@ -1,25 +1,3 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-function emptyFs(path) {
-	let files = []
-	let dir = []
-	if (fs.existsSync(path)) {
-		files = fs.readdirSync(path)
-		files.forEach(file => {
-			const curPath = `${path}/${file}`
-			if (fs.statSync(curPath).isDirectory()) {
-				emptyFs(curPath)
-			} else {
-				fs.unlinkSync(curPath)
-			}
-		})
-		dir = fs.readdirSync(path)
-		dir.forEach(dirName => {
-			fs.rmdirSync(`${path}/${dirName}`)
-		})
-	}
-}
-
-module.exports = {
-	emptyFs,
-}
+module.exports = {}
