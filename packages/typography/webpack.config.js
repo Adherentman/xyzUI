6636f8b0b258d08c098592d0aa142490d945f17c
徐梓昊 <adherentxu@gmail.com>
2018-10-21 23:54:25 +0800

    c

--- packages/typography/webpack.config.js
+++ packages/typography/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-	mode: 'production',
+	mode: 'development',
 	entry: './packages/typography/index.js',
 	output: {
 		path: path.join(__dirname, 'lib'),
