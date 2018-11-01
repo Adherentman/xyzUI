d4548362592299ab925f07ecd6ddcc6437c9c5d2
徐梓昊 <adherentxu@gmail.com>
2018-11-02 00:08:08 +0800

    typography add component prop

--- packages/typography/src/Typography.tsx
+++ packages/typography/src/Typography.tsx
@@ -9,10 +9,24 @@ interface ITypographyProps {
 	 */
 	children: React.ReactNode;
 	/**
+	 * The element
+	 */
+	component?: string | React.ComponentType;
+	/**
 	 * Applies the theme typography styles.
 	 * @default ''
 	 */
-	size?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'title' | 'body' | 'button';
+	size?:
+		| ''
+		| 'h1'
+		| 'h2'
+		| 'h3'
+		| 'h4'
+		| 'h5'
+		| 'h6'
+		| 'title'
+		| 'body'
+		| 'button';
 	/**
 	 * Set the text-align on the component.
 	 * @default inherit
@@ -30,11 +44,18 @@ function capitalize(string: string) {
 	return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-class Typography extends React.PureComponent<ITypographyProps> {
+class Typography extends React.PureComponent<ITypographyProps, {}> {
+	static defaultProps: Partial<ITypographyProps> = {
+		component: 'span',
+		size: '',
+		align: 'inherit',
+	}
+
 	static propTypes = {
 		children: PropTypes.node.isRequired,
 		/** Applies the theme typography styles.  */
 		size: PropTypes.oneOf([
+			'',
 			'h1',
 			'h2',
 			'h3',
@@ -50,8 +71,16 @@ class Typography extends React.PureComponent<ITypographyProps> {
 		/** If true, the text will not wrap, but instead will truncate with an ellipsis. */
 		noWarp: PropTypes.bool,
 	}
+
 	render(): React.ReactNode {
-		const { children, size = '', align = 'inherit', noWarp } = this.props
+		const {
+			children,
+			component: Component,
+			size,
+			align,
+			noWarp,
+			...other
+		} = this.props
 
 		const typographyStyle = classNames(styles.typographyFont, {
 			[styles[`size${capitalize(size)}`]]: size !== '',
@@ -59,9 +88,9 @@ class Typography extends React.PureComponent<ITypographyProps> {
 			[styles.noWarp]: noWarp,
 		})
 		return (
-			<React.Fragment>
-				<span className={typographyStyle}>{children}</span>
-			</React.Fragment>
+			<Component className={typographyStyle} {...other}>
+				{children}
+			</Component>
 		)
 	}
 }
