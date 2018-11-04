e7fc1d0eaa7e66915948635f9173c4326a6be3e7
徐梓昊 <adherentxu@gmail.com>
2018-11-04 14:18:13 +0800

    chore: change css-in-js

--- packages/typography/index.tsx
+++ packages/typography/index.tsx
@@ -44,56 +44,93 @@ function capitalize(str: string) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-class Typography extends React.PureComponent<ITypographyProps, {}> {
-	public static defaultProps: Partial<ITypographyProps> = {
-		component: 'span',
-		size: '',
-		align: 'inherit',
-	};
+const Typography: React.SFC<ITypographyProps> = ({
+	children,
+	component: Component,
+	size,
+	align,
+	noWarp,
+	...other
+}) => {
+	const typographyStyles = classNames(typographyStyle.typographyFont, {
+		[typographyStyle[`size${capitalize(size)}`]]: size !== '',
+		[typographyStyle[`tx${capitalize(align)}`]]: align !== 'inherit',
+		[typographyStyle.noWarp]: noWarp,
+	});
+	return (
+		<Component className={typographyStyles} {...other}>
+			{children}
+		</Component>
+	);
+};
 
-	public static propTypes = {
-		children: PropTypes.node,
-		/** Applies the theme typography styles.  */
-		size: PropTypes.oneOf([
-			'',
-			'h1',
-			'h2',
-			'h3',
-			'h4',
-			'h5',
-			'h6',
-			'title',
-			'body',
-			'button',
-		]),
-		/** Set the text-align on the component. */
-		align: PropTypes.oneOf(['inherit', 'center', 'left', 'right', 'justify']),
-		/** If true, the text will not wrap, but instead will truncate with an ellipsis. */
-		noWarp: PropTypes.bool,
-		component: PropTypes.any,
-	};
+Typography.defaultProps = {
+	component: 'span',
+	size: '',
+	align: 'inherit',
+};
 
-	public render(): React.ReactNode {
-		const {
-			children,
-			component: Component,
-			size,
-			align,
-			noWarp,
-			...other
-		} = this.props;
+// T.propTypes = {
+// 	children: PropTypes.node,
+// 	/** Applies the theme typography styles.  */
+// 	size: PropTypes.any,
+// 	/** Set the text-align on the component. */
+// 	align: PropTypes.oneOf(['inherit', 'center', 'left', 'right', 'justify']),
+// 	/** If true, the text will not wrap, but instead will truncate with an ellipsis. */
+// 	noWarp: PropTypes.bool,
+// 	component: PropTypes.any,
+// };
 
-		const typographyStyles = classNames(typographyStyle.typographyFont, {
-			[typographyStyle[`size${capitalize(size)}`]]: size !== '',
-			[typographyStyle[`tx${capitalize(align)}`]]: align !== 'inherit',
-			[typographyStyle.noWarp]: noWarp,
-		});
-		return (
-			<Component className={typographyStyles} {...other}>
-				{children}
-			</Component>
-		);
-	}
-}
+// class Typography extends React.PureComponent<ITypographyProps, {}> {
+// 	public static defaultProps: Partial<ITypographyProps> = {
+// 		component: 'span',
+// 		size: '',
+// 		align: 'inherit',
+// 	};
+
+// 	public static propTypes = {
+// 		children: PropTypes.node,
+// 		/** Applies the theme typography styles.  */
+// 		size: PropTypes.oneOf([
+// 			'',
+// 			'h1',
+// 			'h2',
+// 			'h3',
+// 			'h4',
+// 			'h5',
+// 			'h6',
+// 			'title',
+// 			'body',
+// 			'button',
+// 		]),
+// 		/** Set the text-align on the component. */
+// 		align: PropTypes.oneOf(['inherit', 'center', 'left', 'right', 'justify']),
+// 		/** If true, the text will not wrap, but instead will truncate with an ellipsis. */
+// 		noWarp: PropTypes.bool,
+// 		component: PropTypes.any,
+// 	};
+
+// 	public render(): React.ReactNode {
+// 		const {
+// 			children,
+// 			component: Component,
+// 			size,
+// 			align,
+// 			noWarp,
+// 			...other
+// 		} = this.props;
+
+// 		const typographyStyles = classNames(typographyStyle.typographyFont, {
+// 			[typographyStyle[`size${capitalize(size)}`]]: size !== '',
+// 			[typographyStyle[`tx${capitalize(align)}`]]: align !== 'inherit',
+// 			[typographyStyle.noWarp]: noWarp,
+// 		});
+// 		return (
+// 			<Component className={typographyStyles} {...other}>
+// 				{children}
+// 			</Component>
+// 		);
+// 	}
+// }
 
 export default Typography;
--- packages/typography/lib/index.d.ts
+++ packages/typography/lib/index.d.ts
@@ -1,42 +1,38 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
 interface ITypographyProps extends React.HTMLAttributes<HTMLElement> {
-    /**
-     * The content of the component.
-     */
-    children: React.ReactNode;
-    /**
-     * The element
-     */
-    component: string | React.ComponentType;
-    /**
-     * Applies the theme typography styles.
-     * @default ''
-     */
-    size: '' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'title' | 'body' | 'button';
-    /**
-     * Set the text-align on the component.
-     * @default inherit
-     */
-    align: string | 'inherit' | 'center' | 'left' | 'right' | 'justify';
-    /**
-     * If true, the text will not wrap, but instead will truncate with an ellipsis.
-     * @default boolean
-     */
-    noWarp: boolean;
+	/**
+	 * The content of the component.
+	 */
+	children: React.ReactNode;
+	/**
+	 * The element
+	 */
+	component: string | React.ComponentType;
+	/**
+	 * Applies the theme typography styles.
+	 * @default ''
+	 */
+	size:
+		| ''
+		| 'h1'
+		| 'h2'
+		| 'h3'
+		| 'h4'
+		| 'h5'
+		| 'h6'
+		| 'title'
+		| 'body'
+		| 'button';
+	/**
+	 * Set the text-align on the component.
+	 * @default inherit
+	 */
+	align: string | 'inherit' | 'center' | 'left' | 'right' | 'justify';
+	/**
+	 * If true, the text will not wrap, but instead will truncate with an ellipsis.
+	 * @default boolean
+	 */
+	noWarp: boolean;
 }
-declare class Typography extends React.PureComponent<ITypographyProps, {}> {
-    static defaultProps: Partial<ITypographyProps>;
-    static propTypes: {
-        children: PropTypes.Requireable<PropTypes.ReactNodeLike>;
-        /** Applies the theme typography styles.  */
-        size: PropTypes.Requireable<string>;
-        /** Set the text-align on the component. */
-        align: PropTypes.Requireable<string>;
-        /** If true, the text will not wrap, but instead will truncate with an ellipsis. */
-        noWarp: PropTypes.Requireable<boolean>;
-        component: PropTypes.Requireable<any>;
-    };
-    render(): React.ReactNode;
-}
+declare const Typography: React.SFC<ITypographyProps>;
 export default Typography;
