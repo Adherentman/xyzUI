c4bf5a178361283bbbb60516c21738b06d049459
徐梓昊 <adherentxu@gmail.com>
2019-04-12 18:23:07 +0800

    docs: add docz site

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -1,8 +1,8 @@
 export const imports = {
   'docz/Home.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docz-home" */ 'docz/Home.mdx'),
-  'docz/components/Button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docz-components-button" */ 'docz/components/Button.mdx'),
+  'docz/components/Box.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docz-components-box" */ 'docz/components/Box.mdx'),
   'docz/components/Typography.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "docz-components-typography" */ 'docz/components/Typography.mdx'),
 }
